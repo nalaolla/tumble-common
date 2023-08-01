a0c0806c8599a5605713f0195fccdaac2ed22d78
daniel,kim <60847008+nalaolla@users.noreply.github.com>
2023-08-01 13:42:40 +0900

    Add files via upload

---
+++ src/test/java/kr/co/tumble/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.tumble.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
